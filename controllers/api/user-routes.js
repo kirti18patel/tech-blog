730883320bf2b95c73246af095bc9aa230003f11
kirti18patel <kirti18patel@yahoo.com>
2021-08-06 09:11:31 -0400

    user get with id api added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,6 +12,26 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post('/', (req, res) => {
   User.create({
     name: req.body.name,
