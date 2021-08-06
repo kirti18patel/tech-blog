842a77f2229d624114cd971cad8da894a7c290ec
kirti18patel <kirti18patel@yahoo.com>
2021-08-06 09:10:12 -0400

    user get api added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post('/', (req, res) => {
   User.create({
     name: req.body.name,
