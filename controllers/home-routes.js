31cf3a9ae901f2d3e1893a9707276f1c263f2a0d
kirti18patel <kirti18patel@yahoo.com>
2021-08-06 18:03:43 -0400

    data displayed in home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const { Post, User} = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('index');
+  Post.findAll({
+    attributes: ['id', 'title', 'description'],
+    include: [
+      {
+        model: User,
+        attributes: ['id','name', 'email']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      console.log(posts);
+      res.render('home', {posts});
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
