c5ff8e63e979806d0dc6f5d0f17c3c1998812596
kirti18patel <kirti18patel@yahoo.com>
2021-08-06 09:08:19 -0400

    user post api added

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', (req, res) => {
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.name = dbUserData.name;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
