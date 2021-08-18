29136e4e137362eed87c5f2bec0c547b35d6370c
kirti18patel <kirti18patel@yahoo.com>
2021-08-17 23:16:04 -0400

    some styling addded

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,4 +1,5 @@
 function addpostShow(){
+  document.querySelector('#addpostShow').classList.add("hide");
     document.querySelector('.addPost').classList.remove("hide");
   }
 
@@ -8,23 +9,27 @@ function addpostShow(){
       const title = document.querySelector("#addPost-title").value;
       const description = document.querySelector("#addPost-content").value;
       
-
-    const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-          description
-        }),
-        headers: {
-          'Content-Type': 'application/json'
+    if(title && description){
+      const response = await fetch(`/api/posts`, {
+          method: 'POST',
+          body: JSON.stringify({
+              title,
+            description
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      
+        if (response.ok) {
+          alert("Post added");
+          location.reload();
+        } else {
+          alert(response.statusText);
         }
-      });
-    
-      if (response.ok) {
-        alert("Post added");
-        location.reload();
-      } else {
-        alert(response.statusText);
+      }
+      else{
+        alert("Enter valid input");
       }
     }
   
